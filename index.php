87bb500fc47abe9cb5c78596d6a257b12bcb7486
DemetrioPadre <demetriopadre@gmail.com>
2024-03-04 17:05:41 +0100

    add comments

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
+//creo una class production 
 class Production
 {
+    //dentro la class production abbiamo le variabili titolo lingua e voto
     public $title;
     public $language;
     public $vote;
 
-    function __construct($title, $language, $vote)
-    {
+
+    //ne determino una funzione per ogni variabile
+    function __construct(
+        string $title,
+        string $language,
+        int $vote
+    ) {
+
+        //richiamiamo le funzioni
         $this->title = $title;
         $this->language = $language;
         $this->vote = $vote;
